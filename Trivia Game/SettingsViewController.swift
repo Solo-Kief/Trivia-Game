2a1003a6eb72ce23d313914c1f6e2a68552aaeea
Solomon Keiffer <solomonkeiffer@MacBook-Pro-80.local>
2018-10-09 14:02:08 -0500

    Built second screen interface. No functionality as of yet.

---
+++ Trivia Game/SettingsViewController.swift
@@ -0,0 +1,26 @@
+//  SettingsViewController.swift
+//  Trivia Game
+//
+//  Created by Solomon Kieffer on 10/9/18.
+//  Copyright © 2018 Phoenix Development. All rights reserved.
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
